0ed74285e8ee23a545f10c99c67ac24013342e61
lcssptz <lcssptz@gmail.com>
2022-12-19 14:43:33 +0800

    Hometask1 (#1)

---
+++ hometask1/src/task2/task2.js
@@ -0,0 +1,21 @@
+// Preset
+const fs = require('fs')
+const csv = require('csvtojson')
+
+const convertCsvToJson = (sour, dest) => {
+  try {
+    const readableStream = fs.createReadStream(sour, 'utf8')
+    const writableStream = fs.createWriteStream(dest, 'utf8')
+
+    readableStream
+      .pipe(csv())
+      .pipe(writableStream)
+  } catch(err) {
+    console.error(JSON.stringify(err))
+  }
+}
+
+const sourCSV = './csvx/nodejs-hw1-ex1.csv'
+const destJSON = './csv/nodejs-hw1-ex1.txt'
+
+convertCsvToJson(sourCSV, destJSON)
---
+++ hometask1/src/utils.js
@@ -0,0 +1,5 @@
+const removeEOL = chunk => chunk.subarray(0, chunk.length - 1)
+
+module.exports = {
+  removeEOL
+}
